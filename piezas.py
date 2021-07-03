78283189fafd21a4a559bdf2fb20ad9d30c9db43
Maucoder <dreg5000.bbb@gmail.com>
2021-07-03 13:44:24 -0400

    Agregando nuevas librerias al proyecto

---
+++ piezas.py
@@ -0,0 +1,16 @@
+import tablero
+
+
+def piezas():
+    tablero.table
+    rey_blanco = {
+        "nombre" : "rey",
+        "position" : "columna_d"[tablero]
+    }
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
