f7541957c748b927a63e20c13a290a6f0da6dc10
Maucoder <dreg5000.bbb@gmail.com>
2021-07-03 13:43:54 -0400

    Mi tablerito y sus piezas v0.1

--- juego.py
+++ juego.py
@@ -1,30 +1,33 @@
+def game():
+    pass
+
 def turnos(player):
     if player == 0:
-        return 0;
+        return 0
     else:
-        return 0;
+        return 1
 
-def tablero():
+# def tablero(): 
 
-    columna_a = [1,2,3,4,5,6,7,8]
-    columna_b = [1,2,3,4,5,6,7,8]
-    columna_c = [1,2,3,4,5,6,7,8]
-    columna_d = [1,2,3,4,5,6,7,8]
-    columna_e = [1,2,3,4,5,6,7,8]
-    columna_f = [1,2,3,4,5,6,7,8]
-    columna_g = [1,2,3,4,5,6,7,8]
-    columna_h = [1,2,3,4,5,6,7,8]
-    
+#     columna_a = [1,2,3,4,5,6,7,8]
+#     columna_b = [1,2,3,4,5,6,7,8]
+#     columna_c = [1,2,3,4,5,6,7,8]
+#     columna_d = [1,2,3,4,5,6,7,8]
+#     columna_e = [1,2,3,4,5,6,7,8]
+#     columna_f = [1,2,3,4,5,6,7,8]
+#     columna_g = [1,2,3,4,5,6,7,8]
+#     columna_h = [1,2,3,4,5,6,7,8]
+
 #Programando nuestro tablero de ajedrez
 
 def run():
-    player = 0;
+    player = 0
     if player == 0:
         print ("Juegan blancas.")
-        player += 1;
+        player += 1
     else:
         print ("Juegan negras")
-        player -= 1;
+        player -= 1
 
 
 if __name__ == "__main__":
