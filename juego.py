c31a407bb917bfe0d6b02955285c0df88ec7e2ed
Maucoder <dreg5000.bbb@gmail.com>
2021-07-03 14:27:27 -0400

    eliminando el ´;´

--- juego.py
+++ juego.py
@@ -1,8 +1,8 @@
 def turnos(player):
     if player == 0:
-        return 0;
+        return 0
     else:
-        return 0;
+        return 0
 
 def tablero():
 
@@ -18,13 +18,13 @@ def tablero():
 #Programando nuestro tablero de ajedrez
 
 def run():
-    player = 0;
+    player = 0
     if player == 0:
         print ("Juegan blancas.")
-        player += 1;
+        player += 1
     else:
         print ("Juegan negras")
-        player -= 1;
+        player -= 1
 
 
 if __name__ == "__main__":
