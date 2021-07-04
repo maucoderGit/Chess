d2be014abbb3e958d6c14e11fc4cea3b6320489a
Maucoder <dreg5000.bbb@gmail.com>
2021-07-03 21:53:35 -0400

    bucle de juego

--- juego.py
+++ juego.py
@@ -1,4 +1,5 @@
 import tablero
+import piezas
 
 
 def game():
@@ -10,15 +11,17 @@ def turnos(player):
     else:
         return 1
 
-
 def run():
+    resultado = ""
     player = 0
-    if player == 0:
-        print ("Juegan blancas.")
-        player += 1
-    else:
-        print ("Juegan negras")
-        player -= 1
+
+    while resultado != "blancas" or resultado != "negras" or resultado != "tablas":
+        if player == 0:
+            print ("Juegan blancas.")
+            player += 1
+        else:
+            print ("Juegan negras")
+            player -= 1
 
 
 if __name__ == "__main__":
---
+++ move.py
@@ -0,0 +1,9 @@
+import piezas
+
+
+def rey():
+    caso = piezas.rey_blanco
+    return caso
+
+objeto = rey()
+print (objeto)
