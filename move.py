465d517e56391c772daefe34b8037f6be26cb298
Maucoder <dreg5000.bbb@gmail.com>
2021-07-10 19:48:14 -0400

    cambiando el algoritmo

--- move.py
+++ move.py
@@ -1,28 +1,8 @@
-import piezas
-
+#import piezas
 # If we use a function, we only need to create move functions and we can continue the game.
 
-def legal_move(before_move, last_move, move_min_max, move_type):
-    jugada = [0,0] 
-    while jugada == [0,0]:
-        if before_move[0] - last_move[0] > move_min_max[0]:
-            print("this move is not valid")
-            jugada[0] = 0
-        else:
-            columna = before_move[0]
-            jugada[0] = 1
-
-        if before_move[1] - last_move[1] > move_min_max[1]:
-            print("this move is not correct")
-            jugada[1] = 0
-        else:
-            fila = before_move[1]
-            jugada[1] = 1
-        
-        nueva_posicion = [columna, fila]
-    
-    return nueva_posicion
-
+def legal_move(columna_anterior,columna_actual , fila_anterior, fila_actual, move_min_max):
+    print ("Hola :)")
 def up_down(last_move, move_min_max, before_move):
     pass
 
@@ -31,6 +11,11 @@ def left_right(columna, fila):
     nueva_posicion = [columna, fila]
     return nueva_posicion
 
+columna = int(input("cual es la columna: "))
+fila = int(input("cual es la fila: "))
+culumna_ant = 1
+fila_ant = 1
+legal_move(culumna_ant,columna,fila, fila_ant , move_min_max=1)
 #Elimine los parametros 'movimiento_1' 'movimiento_2'
 
 def diagonales(columna, fila):
